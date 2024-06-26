7b49a9d8373201937ee60dbe67f9fdf86897bbd8
#SONY_FOX <131622618+brayanop@users.noreply.github.com>
2024-06-26 14:40:45 -0500

    Create Proyecto_github.cpp

---
+++ Proyecto_github.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+string Computer() {
+    int valor = rand() % 3;
+    if (valor == 0){
+        return "piedra";
+	}else if (valor == 1){
+        return "papel";
+	}else{
+        return "tijeras";
+    }
+}
+
+string ganador(string usuario, string computer) {
+    if (usuario == computer){
+        return "Empate";
+	} else if ((usuario == "piedra" && computer == "tijeras") ||
+             (usuario == "papel" && computer == "piedra") ||
+             (usuario == "tijeras" && computer == "papel")){
+        return "Usuario";
+	}else{
+        return "Computadora";
+}
+}
+
+int main() {
+    srand(20);
+    int user_g = 0;
+    int computer_g = 0;
+    
+    while (user_g < 3 && computer_g < 3) {
+        string jugador;
+        cout << "Elige piedra, papel o tijeras (Escribe): ";
+        cin >> jugador;
+
+        if (jugador != "piedra" && jugador!= "papel" && jugador != "tijeras") {
+            cout << "Eleccion Invalida.....(Intente de nuevo)" << endl;
+            continue;
+        }
+
+        string maquina = Computer();
+
+        cout << "Usuario: " << jugador << endl;
+        cout << "Computadora : " << maquina << endl;
+
+        string winner = ganador(jugador,maquina);
+
+        if (winner == "Usuario") {
+            user_g++;
+        } else if (winner == "Computadora") {
+            computer_g++;
+        } else {
+            cout << "Es un empate!" << endl;
+        }
+        cout << "Marcador: Usuario " << user_g << " - " << computer_g<< " Computadora" << endl;
+        cout << "****************************************************" << endl;
+    }
+
+    if (user_g == 3) {
+        cout << ".........WINNER........." << endl;
+    } else {
+        cout << "........GAME OVER......." << endl;
+    }
+
+    return 0;
+}
